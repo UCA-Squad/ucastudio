86c1e0637b5f4644c7f077ee87c89182355411ab
Yoann Raison <yoann.raison@uca.fr>
2019-04-03 12:02:14 +0200

    comment SIGINT kill ffmpeg

--- server.js
+++ server.js
@@ -108,6 +108,7 @@ io.on('connection', function(socket){
 	socket.on('binarystream',function(m){
 		if(!feedStream){
 			socket.emit('fatal','rtmp not set yet.');
+			ffmpeg_process.stdin.end();
 			return;
 		}
 		feedStream(m);
@@ -117,7 +118,7 @@ io.on('connection', function(socket){
 		if(ffmpeg_process) {
 			try {
 				ffmpeg_process.stdin.end();
-				ffmpeg_process.kill('SIGINT');
+				// ffmpeg_process.kill('SIGINT');
 			} catch (e) {console.warn('killing ffmoeg process attempt failed...');}
 		}
 	});
@@ -126,7 +127,7 @@ io.on('connection', function(socket){
 		if(ffmpeg_process)
 		try{
 			ffmpeg_process.stdin.end();
-			ffmpeg_process.kill('SIGINT');
+			// ffmpeg_process.kill('SIGINT');
 		}catch(e){console.warn('killing ffmoeg process attempt failed...');}
 	});
 	socket.on('error',function(e){
