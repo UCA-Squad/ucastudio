13865da5fdf212bf531074598d01d07b883c8334
RAISON Yoann <yoann.raison@uca.fr>
2020-04-09 17:07:59 +0200

    udpate script clean folder

--- cleanFolderRecords.sh
+++ cleanFolderRecords.sh
@@ -1,7 +1,2 @@
 STATIC_RECORD_FOLDER=./static/records/ucastudio/*/*
-seven_days=$(date -d '7 days ago' +%s)
-for f in $STATIC_RECORD_FOLDER; do
-   [ -d "$f" ] || continue 
-   timestampCurrentFolder=$(stat -c '%Y' $f)
-   (( timestampCurrentFolder <  seven_days)) && rm -rf "$f"
-done
+find $STATIC_RECORD_FOLDER -type d -mtime +7 -exec rm -Rf {} \;
