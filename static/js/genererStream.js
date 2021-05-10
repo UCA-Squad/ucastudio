0904084e24895f4ad47382a628a99abc9eb24a4a
Raison Yoann <yoann.raison@uca.fr>
2021-05-10 11:44:10 +0200

    [moodle] modif live

--- static/js/genererStream.js
+++ static/js/genererStream.js
@@ -15,7 +15,7 @@ function genLive() {
 	}
 
 	var courseid = $_GET("courseid");
-	var serieid = $_GET("serieid");
+	var email = $_GET("email");
 	var data = null;
 
 	var xhr = new XMLHttpRequest();
@@ -27,13 +27,11 @@ function genLive() {
 			    }
 	});
 	var moodle = $('#moodle').val();
-	xhr.open("POST", moodle+"insertLive.php?courseid="+courseid+"&serieid="+serieid);
+	xhr.open("POST", moodle+"insertLive.php?courseid="+courseid+"&email="+email);
 	xhr.setRequestHeader("User-Agent", "PostmanRuntime/7.13.0");
 	xhr.setRequestHeader("Accept", "*/*");
 	xhr.setRequestHeader("Cache-Control", "no-cache");
-	xhr.setRequestHeader("Postman-Token", "643fc522-2791-4d29-be77-a179189985b8,19266315-cc2d-42f2-932e-f98d208e00c1");
 	xhr.setRequestHeader("Host", "ent.uca.fr");
-	xhr.setRequestHeader("Cookie", "MoodleSession=giruj0vbh61j1vi2r837lc7ep8; ROUTEMOODLE=.node1");
 	xhr.setRequestHeader("accept-encoding", "gzip, deflate");
 	xhr.setRequestHeader("content-length", "");
 	xhr.setRequestHeader("Connection", "keep-alive");
